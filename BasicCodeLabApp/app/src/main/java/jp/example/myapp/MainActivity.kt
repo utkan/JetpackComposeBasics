32086cf5dddb250dd894869e9f17549f966153bd
Utkan <usargin@ambidexter.gmbh>
2020-11-18 15:06:36 +0100

    init

---
+++ BasicCodeLabApp/app/src/main/java/jp/example/myapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package jp.example.myapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
+import jp.example.myapp.ui.MyComposeApplicationTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyComposeApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyComposeApplicationTheme {
+        Greeting("Android")
+    }
+}
